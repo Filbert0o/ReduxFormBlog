fd0b30abcdd03b086ec53f3334eb0091dbb40353
Timothy Andrew Filbert <timfilbert97@gmail.com>
2018-02-02 09:13:53 -0500

    handle new post submit

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,24 +1,42 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions/index';
 
 class PostsNew extends Component {
   renderField(field) {
+    // es6 syntax to replace 'field.meta.touched', 'field.meta.error'
+    // with 'touched' and 'error' accordingly
+    const { meta: { touched, error } } = field;
+    const className = `form-group ${touched && error ? 'has-danger' : ''}`
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input
           className="form-control"
           type="text"
           {...field.input}
         />
-        {field.meta.error}
+        <div className="text-help">
+          {touched ? error : ''}
+        </div>
       </div>
     )
   }
 
+  onSubmit(values) {
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
+  }
+
   render() {
+    const { handleSubmit } = this.props;
+
     return (
-      <form>
+      <form onSubmit={ handleSubmit(this.onSubmit.bind(this)) }>
         <Field
           label="Title"
           name="title"
@@ -35,6 +53,7 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <button type="submit" className="btn btn-primary">Submit</button>
+        <Link to="/" className="btn btn-danger">Cancel</Link>
       </form>
     );
   }
@@ -63,4 +82,6 @@ function validate(values) {
 export default reduxForm({
   validate, // validate: validate
   form: 'PostsNewForm'
-})(PostsNew);
+})(
+  connect(null, { createPost })(PostsNew)
+);
